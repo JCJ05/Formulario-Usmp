97456fc2ec6271e27d311f4e6145add5bd26a9ed
JCJ05 <julitoaguerorengifo@hotmail.com>
2021-08-25 16:14:49 -0500

    Panel de Trabajo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Formulario_Usmp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
